f42701f7d4c37f4cd72f07702196d22406753c5e
Carlos <carlosaac232001@gmail.com>
2026-04-23 22:06:45 -0500

    feat: create main func to create table, add dependencies and run server

--- main.go
+++ main.go
@@ -1,1 +1,55 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/Carlosaac23/go-bookstore/internal/service"
+	"github.com/Carlosaac23/go-bookstore/internal/store"
+	"github.com/Carlosaac23/go-bookstore/internal/transport"
+)
+
+func main() {
+	// Connect to SQLite
+	db, err := sql.Open("sqlite3", "./books.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create table (books) if no exists
+	q := `
+		CREATE TABLE IF NOT EXISTS books (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			title TEXT NOT NULL,
+			author TEXT NOT NULL
+		)
+	`
+	if _, err := db.Exec(q); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Add dependencies
+	bookStore := store.New(db)
+	bookService := service.New(bookStore)
+	bookHandler := transport.New(bookService)
+
+	// Config routes
+	http.HandleFunc("/books", bookHandler.HandleBooks)
+	http.HandleFunc("/books/", bookHandler.HandleBookByID)
+
+	fmt.Println("Server running at http://localhost:8000")
+	fmt.Println("API Endpoints:")
+	fmt.Println("  GET     /books       - Get all books")
+	fmt.Println("  POST    /books       - Create a new book")
+	fmt.Println("  GET     /books/{id}  - Get a specific book")
+	fmt.Println("  PUT     /books/{id}  - Update a book")
+	fmt.Println("  DELETE  /books/{id}  - Delete a book")
+
+	// Run and listen server
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
